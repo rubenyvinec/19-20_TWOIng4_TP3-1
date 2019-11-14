260d0679253b8a4ebd673150ea522b50cc1c9c7b
Ruben <yvinecman@gmail.com>
2019-11-14 19:28:22 +0100

    Début du tp

---
+++ tp4/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+// MES DONNEES
+
+const profil = [
+  {
+    nom :"Debussy",
+    prenom : "Claude",
+    date :"01/01/2001",
+    img :''
+  },
+  {
+    nom :"Martine",
+    prenom : "Dubpos",
+    date :"04/07/1990",
+    img :''
+},
+{
+    nom :"Jeanne",
+    prenom : "Parent",
+    date :"27/08/1990",
+    img :''
+    }
+];
+
+
+
+//export default App;
