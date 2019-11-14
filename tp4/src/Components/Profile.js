e1310272e08870f44f0ce31f36f7c892a4ae0388
Ruben <yvinecman@gmail.com>
2019-11-14 23:53:01 +0100

    CSS pour les noms

--- tp4/src/Components/Profile.js
+++ tp4/src/Components/Profile.js
@@ -27,7 +27,7 @@ export default class Profile extends Component {
     render() {
         return(
             <div style={style[this.state.style]}>
-                <img style={{flex:1,    width: 80, height:100,borderRadius: "50%",justifyContent:"center" }} src={this.props.profil.img} />
+                <img style={{flex:1, position: "relative", left: "40%", top:"10%",   width: 80, height:100,borderRadius: "50%", }} src={this.props.profil.img} />
                 <p>Prenom : {this.props.profil.prenom}  <space style={{float : 'right',paddingRight:80,}}> Nom : {this.props.profil.nom} </space> </p>
                 <p>Date de Naissance: {this.props.profil.naissance}</p>
                 <p>Dernier Post : {this.props.profil.post} </p>
--- tp4/src/Components/ProfileButton.js
+++ tp4/src/Components/ProfileButton.js
@@ -4,7 +4,7 @@ export default class Profile extends Component {
 
     render() {
         return(
-            <div style={{display: 'flex', width: 200, height: 50, justifyContent: 'center', alignItems: 'center', border: '2px solid black', }}>
+            <div style={{display: 'flex', width: 200, height: 50, justifyContent: 'center', alignItems: 'center', border: '2px solid black', position:"relative",left:"1200px",top :"20%" }}>
                 <p>{this.props.nom}</p>
 
             </div>
